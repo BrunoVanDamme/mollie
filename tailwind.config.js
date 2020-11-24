345608a01bf8838f3e7569ea88e9883bd198be0d
Bruno Van Damme <vof.heights@gmail.com>
2020-11-24 17:35:52 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
