b3cff02e1b64ac3d7a1c2c7e6b4673a5e0848c2a
Bruno Van Damme <vof.heights@gmail.com>
2020-11-24 19:58:36 +0100

    Initial commit

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="text-lg">Starter template with ES Linter, Prettier and Tailwind configured.</div>;
-}
---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="mt-5 text-lg">Starter template with ES Linter, Prettier, Tailwind and Typescript configured.</div>;
+}
